04434b61a0a70dda629654e111fa596080024904
Matt Jamison <mmjamiso@kent.edu>
2017-12-08 21:54:18 -0500

    working league show blade

--- resources/views/leagues/show.blade.php
+++ resources/views/leagues/show.blade.php
@@ -1,17 +1,13 @@
 @extends('layouts.app')
 @section('main')
 
-<h1>{{ $ponger->first_name }}</h1>
+<h1>{{ $league->name }}</h1>
+	
 	<ul>
-		<li>Ponger first_name: {{ $ponger->first_name }}</li>
-		<li>Ponger last_name: {{ $ponger->last_name }}</li>
-		<li>Ponger city: {{ $ponger->city }}</li>
-		<li>Ponger state: {{ $ponger->state }}</li>
-		<li>Ponger age: {{ $ponger->age }}</li>
-		<li>Ponger swing_hand: {{ $ponger->swing_hand }}</li>
-		<li>Ponger lifetime_win: {{ $ponger->lifetime_win }}</li>
-		<li>Ponger lifetime_loss: {{ $ponger->lifetime_loss }}</li>
+ 		<li> {{ $league->name }} </li>
+ 		<li> {{ $league->commisioner }}</li>
 	</ul>
+
 <span>
 	<a href="{{ route('leagues.edit', $league) }}" class="btn btn-primary">Edit</a>
 </span>
