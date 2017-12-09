c290d199ebb269ad9e8cc8814e1d2f74c56927e3
Matt Jamison <mmjamiso@kent.edu>
2017-12-08 22:25:12 -0500

    Link to Leagues index page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,9 @@
       <p class="navbar-text">
         <a class="navbar-link" href="{{ route('pongers.index') }}">PONGERS</a>
       </p>
+            <p class="navbar-text">
+        <a class="navbar-link" href="{{ route('leagues.index') }}">LEAGUES</a>
+      </p>
    </div>
 </nav>
 
