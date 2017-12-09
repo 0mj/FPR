99fb399c9cb7041302e04ce0e610006f6f3ba00b
Matt Jamison <mmjamiso@kent.edu>
2017-12-08 22:32:38 -0500

    INPUT NAME!!

--- resources/views/pongers/edit.blade.php
+++ resources/views/pongers/edit.blade.php
@@ -5,32 +5,33 @@
 
 
 <form action="{{ url('pongers', $ponger) }}" method="post">
+	<!-- <form action="{{ route('pongers.update', $ponger) }}" method="post"> -->
 	{{ csrf_field() }}
 	{{ method_field('patch') }}
 
 	<label for="first_name">Ponger First Name</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->first_name }}">
+	<input type="text" class="form-control" name="first_name" value="{{ $ponger->first_name }}">
 
 	<label for="last_name">Ponger Last Name</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->last_name }}">
+	<input type="text" class="form-control" name="last_name" value="{{ $ponger->last_name }}">
 
 	<label for="city">Ponger City</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->city }}">
+	<input type="text" class="form-control" name="city" value="{{ $ponger->city }}">
 
 	<label for="state">Ponger State</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->state }}">
+	<input type="text" class="form-control" name="state" value="{{ $ponger->state }}">
 
 	<label for="age">Ponger Age</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->age }}">
+	<input type="text" class="form-control" name="age" value="{{ $ponger->age }}">
 
 	<label for="swing_hand">Ponger Swing Hand</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->swing_hand }}">
+	<input type="text" class="form-control" name="swing_hand" value="{{ $ponger->swing_hand }}">
 
 	<label for="lifetime_win">Ponger Life time wins</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->lifetime_win }}">
+	<input type="text" class="form-control" name="lifetime_win" value="{{ $ponger->lifetime_win }}">
 
 	<label for="lifetime_loss">Ponger Life time Losses</label>
-	<input type="text" class="form-control" name="name" value="{{ $ponger->lifetime_loss }}">
+	<input type="text" class="form-control" name="lifetime_loss" value="{{ $ponger->lifetime_loss }}">
 
 	<button type="submit" class="btn btn-primary">
 	    Update
