34a3ddbdf3fb8895660ae4e3cd22f2ee09ed29da
Matt Jamison <mmjamiso@kent.edu>
2017-12-11 14:23:13 -0500

    Bracket Controller created

---
+++ app/Http/Controllers/BracketController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BracketController extends Controller
+{
+    
+    public function index()
+    {
+    	// return 'BracketController index!!!';
+    	return view('pongbracket.bracket');
+    }
+
+
+
+}
