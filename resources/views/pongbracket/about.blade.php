445b5eeba6cd46634d3e1193acc8cda3f7bbd1d5
Matt Jamison <mmjamiso@kent.edu>
2017-12-11 14:58:17 -0500

    About bracket page.

---
+++ resources/views/pongbracket/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('main')
+
+    <div class="row">
+        <div class="col">
+            
+            <h1>About the Family Pong Record Bracket</h1>
+            <p>Information regarding the bracket..</p>
+        </div>
+
+    </div>
+
+@endsection
