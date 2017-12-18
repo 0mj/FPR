0a2e7612b7bf2de789a563e10fea3a6d18b91c15
Matt Jamison <mmjamiso@kent.edu>
2017-12-18 15:00:24 -0500

    Delete option added

--- resources/views/pongers/index.blade.php
+++ resources/views/pongers/index.blade.php
@@ -2,18 +2,30 @@
 @section('main')
 
 
+<div class="row">
+    <div class="col-md-4">
 
-<ul>
-	@foreach($pongers as $ponger)
-		<li>
-			<a href="{{ route('pongers.show', $ponger) }}">
-				{{ $ponger->first_name . ' ' . $ponger->last_name . ' ' . $ponger->state }}
-			</a>
-		</li>
-	@endforeach
-</ul>
+	<ul>
+		@foreach($pongers as $ponger)
+			<li>
+				<a href="{{ route('pongers.show', $ponger) }}">
+					{{ $ponger->first_name . ' ' . $ponger->last_name . ' ' . $ponger->state }}
+				</a>
+				<small><a href="{{ route('pongers.edit', $ponger) }}">edit</a></small>
+			</li>
+		@endforeach
+	</ul>
 
-{{ $pongers->links() }}
+	{{ $pongers->links() }}
 
+	</div>
+
+	<div class="col-md-4"></div>
+	<div class="col-md-4">
+		
+		<a href="{{route('pongers.create')}} ">Create New Ponger</a>
+	</div
+
+</div>
 
 @endsection
