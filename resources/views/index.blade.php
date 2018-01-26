9bc8d794ec2f203becc82d2a9b87932da3e5c971
Matt Jamison <mmjamiso@kent.edu>
2018-01-26 09:03:32 -0500

    ignore

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,27 @@
 @section('main')
 
     <div class="row">
-        <div class="col">
+        <div class="col-md-6">
             
             <h1>Friends of Pong</h1>
-            <p>Ping Pong leagues, players, rankings and more.</p>
-            <pre>
-            Lorem ipsum dolor amet next level migas crucifix flexitarian glossier
-            tumeric try-hard cloud bread lyft forage. Cloud bread squid pork belly
-            glossier organic poke pickled beard. Squid intelligentsia meditation
-            pabst, tote bag mixtape messenger bag ramps YOLO deep v. Messenger bag
-            kogi narwhal hella, man bun street art godard kombucha vaporware neutra
-            hoodie pickled VHS skateboard.	
-            </pre>
+            <p>Ping Pong "LEAGUES", "PONGERS", rankings and more.</p>
+                        
+            <p>Friends of Pong is a demonstration of my full stack capabilities.
+            This site was stood up using heroku (AWS), Laravel PHP Framework,
+            MySQL, jQuery, and bootstrap.</p>
+
+            <p>It is fully functional allowing users the ability to create,
+            update, read and delete (CRUD) "PONGERS" and "LEAGUES".</p>
+
+            <p>Within the "LEAGUES" section you can associate "PONGERS" to a league
+            by selecting the radio box next to their name and then selecting
+            update at the bottom. Revisit that league again and you will see
+            the changes saved.</p>
+
+            <p>Give it a shot.  Add a ponger.  Then add them to a league.
+            Delete them etc.. </p>
+
+            
         </div>
 
     </div>
