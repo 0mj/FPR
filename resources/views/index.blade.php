1dad9ef0de0a3c639b08bf1cbf14f5f7531160b4
Matt Jamison <mmjamiso@kent.edu>
2018-01-26 07:51:32 -0500

    lorem ipsum

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col">
             
-            <h1>Family Pong Record</h1>
-            <p>Legendary pong record repository.  Official proof for bragging rights.</p>
+            <h1>Friends of Pong</h1>
+            <p>Ping Pong leagues, players, rankings and more.</p>
             <pre>
+            Lorem ipsum dolor amet next level migas crucifix flexitarian glossier tumeric try-hard cloud bread lyft forage. Cloud bread squid pork belly glossier organic poke pickled beard. Squid intelligentsia meditation pabst, tote bag mixtape messenger bag ramps YOLO deep v. Messenger bag kogi narwhal hella, man bun street art godard kombucha vaporware neutra hoodie pickled VHS skateboard.	
             	
-            	flex box <a href="http://getbootstrap.com/docs/3.3/examples/grid/">bootstrap 3.3.7</a> on Bracket page
                 
             </pre>
         </div>
