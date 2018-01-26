b7f13684332f0e2ff7e80cb2c92925a7e5d801ae
Matt Jamison <mmjamiso@kent.edu>
2018-01-26 09:05:49 -0500

    ignore

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,20 +6,11 @@
         <div class="col-md-6">
             
             <h1>Friends of Pong</h1>
-            <p>Ping Pong "LEAGUES", "PONGERS", rankings and more.</p>
+            <blockquote>Ping Pong "LEAGUES", "PONGERS", rankings and more.</blockquote>
                         
-            <p>Friends of Pong is a demonstration of my full stack capabilities.
-            This site was stood up using heroku (AWS), Laravel PHP Framework,
-            MySQL, jQuery, and bootstrap.</p>
-
-            <p>It is fully functional allowing users the ability to create,
-            update, read and delete (CRUD) "PONGERS" and "LEAGUES".</p>
-
-            <p>Within the "LEAGUES" section you can associate "PONGERS" to a league
-            by selecting the radio box next to their name and then selecting
-            update at the bottom. Revisit that league again and you will see
-            the changes saved.</p>
-
+            <p>Friends of Pong is a demonstration of my full stack capabilities. This site was stood up using Heroku (AWS), Laravel PHP Framework, MySQL, jQuery, and Bootstrap.</p>
+            <p>It is fully functional allowing users the ability to create, update, read and delete (CRUD) pongers and leagues.</p>
+            <p>Within the "LEAGUES" section you can associate "PONGERS" to a league by selecting the radio box next to their name. Then select the update button at the bottom. Revisit that league again and you will see the changes saved.</p>
             <p>Give it a shot.  Add a ponger.  Then add them to a league.
             Delete them etc.. </p>
 
