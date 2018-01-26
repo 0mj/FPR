17d383b5beb05afa1a7004452081394c223d86e3
Matt Jamison <mmjamiso@kent.edu>
2018-01-26 07:26:11 -0500

    remove commented code

--- config/database.php
+++ config/database.php
@@ -1,12 +1,5 @@
 <?php
     
-    // $url = getenv('JAWSDB_URL');
-    // $dbparts = parse_url($url);
-
-    // $hostname = $dbparts['host'];
-    // $username = $dbparts['user'];
-    // $password = $dbparts['pass'];
-    // $database = ltrim($dbparts['path'],'/');
 $dbUrl = parse_url(env("JAWSDB_URL"));    
 
 return [
@@ -23,7 +16,7 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
-    // 'default' => env('JAWSDB_URL', 'mysql'),
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -64,18 +57,18 @@ return [
         //     'engine' => null,
         // ],
         'mysql' => [
-    'driver' => 'mysql',
-    'host' => isset($dbUrl["host"]) ? $dbUrl["host"] : env("DB_HOST"),
-    'port' => isset($dbUrl["port"]) ? $dbUrl["port"] : env("DB_PORT"),
-    'database' => isset($dbUrl["path"]) ? ltrim($dbUrl["path"], '/') : env("DB_DATABASE"),
-    'username' => isset($dbUrl["user"]) ? $dbUrl["user"] : env("DB_USERNAME"),
-    'password' => isset($dbUrl["pass"]) ? $dbUrl["pass"] : env("DB_PASSWORD"),
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
-    'prefix' => '',
-    'strict' => true,
-    'engine' => null,
-],
+            'driver' => 'mysql',
+            'host' => isset($dbUrl["host"]) ? $dbUrl["host"] : env("DB_HOST"),
+            'port' => isset($dbUrl["port"]) ? $dbUrl["port"] : env("DB_PORT"),
+            'database' => isset($dbUrl["path"]) ? ltrim($dbUrl["path"], '/') : env("DB_DATABASE"),
+            'username' => isset($dbUrl["user"]) ? $dbUrl["user"] : env("DB_USERNAME"),
+            'password' => isset($dbUrl["pass"]) ? $dbUrl["pass"] : env("DB_PASSWORD"),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
