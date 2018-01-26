898577a9008bb7765df420071fb2df792541c8d5
Matt Jamison <mmjamiso@kent.edu>
2018-01-26 11:06:53 -0500

    github link added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
             <h1>Friends of Pong</h1>
             <blockquote>Ping Pong "LEAGUES", "PONGERS", rankings and more.</blockquote>
                         
-            <p>Friends of Pong is a demonstration of my full stack capabilities. This site was stood up using Heroku (AWS), Laravel PHP Framework, MySQL, jQuery, and Bootstrap.</p>
+            <p>Friends of Pong is a demonstration of my full stack capabilities. This site was stood up using Heroku (AWS), Git, Laravel PHP Framework, MySQL, jQuery, and Bootstrap. Code on <a href="https://github.com/0mj/FPR">Github</a>.</p>
             <p>It is fully functional allowing users the ability to create, update, read and delete (CRUD) pongers and leagues.</p>
             <p>Within the "LEAGUES" section you can associate "PONGERS" to a league by selecting the radio box next to their name. Then select the update button at the bottom. Revisit that league again and you will see the changes saved.</p>
             <p>Give it a shot.  Add a ponger.  Then add them to a league.
