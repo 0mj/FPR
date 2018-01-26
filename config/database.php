f0e96f497daaab1375ec65f6d3b8bc9d2c165591
Matt Jamison <mmjamiso@kent.edu>
2018-01-25 20:10:49 -0500

    hacking at the db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
+    
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
 
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+    
 return [
 
     /*
@@ -42,11 +50,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $hostname),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
