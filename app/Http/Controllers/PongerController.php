d86f1ed7afd1bc2762c01be7a680864ec421c696
Matt Jamison <mmjamiso@kent.edu>
2018-01-30 15:50:43 -0500

    debug false and footer info color

--- app/Http/Controllers/PongerController.php
+++ app/Http/Controllers/PongerController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Ponger;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
 class PongerController extends Controller
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<footer class="footer">
+<footer class="footer bg-info">
     <div class="container">
         <span class="text-muted">{{ date('Y-M-d h:i A',strtotime('now'))}} UTC - Friends of Pong.</span>
     </div>
