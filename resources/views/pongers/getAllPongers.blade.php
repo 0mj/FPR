ee009e3cebe09aac6e8eed3eb92282ef922abf80
Matt Jamison <mmjamiso@kent.edu>
2018-02-02 10:01:30 -0500

    better looking cards

--- resources/views/pongers/getAllPongers.blade.php
+++ resources/views/pongers/getAllPongers.blade.php
@@ -4,6 +4,13 @@
 
 
 
-<h1 class="ml-1">PONGERS</h1>
+<!-- <iframe src="https://calendar.google.com/calendar/embed?src=thirdshiftvideo%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="100%" height="1000" frameborder="0" scrolling="no"></iframe> -->
+
+
+@php 
+phpinfo();
+
+
+@endphp
     
 @endsection
--- resources/views/pongers/index.blade.php
+++ resources/views/pongers/index.blade.php
@@ -4,7 +4,7 @@
 
 
 
-<h1 class="ml-1">PONGERS</h1>
+<h1 class="ml-1">meet the PONGERS</h1>
     <div class="row justify-content-center">
         @foreach ($pongers as $ponger)
             @php
@@ -12,21 +12,28 @@
             $imageDataUri = $identicon->getImageDataUri($ponger->id);
             @endphp
 
-            <section class="col-md-4 pb-3">
-                <div class="card">
-                    <a href="{{ route('pongers.show', $ponger) }}">
-                    	<img class="card-img-top p-2" src="{{ $imageDataUri }}" height="150" width="150" />
-                    </a>
-                    <ul class="card-block list-unstyled">
-                        <li class="pull-left">
-                            <a href="{{ route('pongers.show', $ponger) }}">
-                                {{ $ponger->first_name . ' ' . $ponger->last_name }}
-                            </a>
-                            
-                        </li>
-                      
-                    </ul>
-                </div>
+        <section class="col-md-4">
+            
+            <div class="panel panel-primary">
+              <!-- Default panel contents -->
+              <div class="panel-heading">{{ $ponger->first_name . ' ' . $ponger->last_name }}</div>
+              <div class="panel-body">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="{{ route('pongers.show', $ponger) }}">
+                            <img class="card-img-top p-2" src="{{ $imageDataUri }}" height="150" width="150" />
+                        </a>
+                    </li>
+                    <li class="list-group-item">{{  $ponger->city . ' ' . $ponger->state }}</li>
+                    <li class="list-group-item">{{ $ponger->swing_hand . ' handed. Age: '  . $ponger->age }}</li>
+                    <li class="list-group-item">Wins: {{ $ponger->lifetime_win . ' Losses: ' . $ponger->lifetime_loss }}</li>
+                    <li class="list-group-item"><a href="{{ route('pongers.show', $ponger) }}"> {{ $ponger->first_name . ' ' . $ponger->last_name }} </a> Lorem ipsum dolor amet godard occupy church-key, williamsburg ugh hammock selvage vape umami af kickstarter adaptogen wayfarers actually. Neutra farm-to-table cornhole, prism 3 wolf moon typewriter occupy listicle scenester franzen iPhone chillwave gentrify live-edge. Snackwave waistcoat twee kale chips, kombucha selvage venmo distillery pinterest street art vape before they sold out flannel photo booth. Truffaut lomo banh mi bicycle rights listicle, hella fixie keytar VHS.</li>
+                   
+
+                </ul>
+            </div>
+
+            </div>
             </section>
         @endforeach
         {{ $pongers->links('vendor.pagination.bootstrap-4') }}
