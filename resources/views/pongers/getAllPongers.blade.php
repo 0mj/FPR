b4a98f8b8b26b87a0cbf7ab25d21a59cc61bf35c
Matt Jamison <mmjamiso@kent.edu>
2018-02-07 07:33:20 -0500

    cleanup

--- resources/views/pongers/getAllPongers.blade.php
+++ resources/views/pongers/getAllPongers.blade.php
@@ -4,8 +4,6 @@
 
 
 
-<!-- <iframe src="https://calendar.google.com/calendar/embed?src=thirdshiftvideo%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="100%" height="1000" frameborder="0" scrolling="no"></iframe> -->
-
           
 @php
 $json_string = file_get_contents("http://api.wunderground.com/api/a42738d57e012c83/geolookup/conditions/q/OH/Twinsburg.json");
