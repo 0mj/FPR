bc7270974aeb091d05876a91bd6797283a5be703
Matt Jamison <mmjamiso@kent.edu>
2018-02-07 10:38:06 -0500

    bootstrap javascript to enable carousel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,5 +38,6 @@
 @include('partials.footer')
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="{{ asset('js/app.js')}}"></script>
   </body>
 </html>
