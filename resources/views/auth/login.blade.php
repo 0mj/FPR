3d570092f76ea851f6b54115dc67932afdacf1c9
Matt Jamison <mmjamiso@kent.edu>
2018-02-19 16:22:55 -0500

    alert message on fail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,13 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            @if(Session::has('fail'))
+            <div class="alert alert-danger">
+                {{ Session::get('fail') }}
+
+            </div>
+            @endif
+
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
 
