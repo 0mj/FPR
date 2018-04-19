47af3bc8927c08ab6463363607e61b59312b6fa3
Matt Jamison <mmjamiso@kent.edu>
2018-04-19 14:05:53 -0400

    learn more

---
+++ resources/views/partials/public-profile-courses-completed.blade.php
@@ -0,0 +1,9 @@
+<p>
+    Achievements:<br>
+     <a href="{{ URL::to('/badgeviewer') }}">CodeSchool.com courses completed</a><br>
+     <a href="https://www.codecademy.com/users/0mj/achievements">CodeCademy.com achievements</a><br>
+     <!-- <a href="//lynda.com">lynda.com courses completed</a><br> -->
+     <a href="https://app.pluralsight.com/profile/MattJFM">Pluralsight.com courses completed</a><br>
+
+</p>
+
