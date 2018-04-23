05764ba100e11bd2424dc623ec5570a2923dc821
Matt Jamison <mmjamiso@kent.edu>
2018-04-23 14:05:58 -0400

    amount of teams that get a buy or a bye

--- public/test_bracket_build/form/logic.php
+++ public/test_bracket_build/form/logic.php
@@ -1,29 +1,40 @@
 <?php
-	$a = $_GET['amount']; 
-	
-mypr(whatWeNeedToKnow($a));
+	/*
+			ideal bracket numbers: 128,64,32,16,8,4
+			ideal bracket - participants = buys
+			example: 16 - 13 = 3
 
-	function whatWeNeedToKnow($amount_of_teams){
-		/*
 			The number of people or teams who should sit out the first round
 			should always equal the difference between the number of participants
-			and the total number of spaces in the next largest bracket.
+			and the total number of spaces in the next largest bracket. For
+			instance, if thirteen people want to participate in a tournament,
+			assume a bracket of sixteen, with three vacant spaces. The difference
+			between the number of available spaces and the number of participants
+			is three, so three people should sit out the first round.
+
+			The result is exactly the same as if those three people had played
+			and won their first-round matches. The other ten participants will be
+			reduced to five by the second round, which added to those who received
+			a first-round "bye" leaves exactly eight players in the second round,
+			which is the ideal number.
 			CREDIT: https://thejawaclan.com/topic/6765-how-to-make-tournament-brackets-for-uneven-numbers-of-people/
 		*/
+
+
+	$a = $_GET['amount']; mypr(whatWeNeedToKnow($a));
+
+	function whatWeNeedToKnow($amount_of_teams){
 		
 		$return = [];
-		// $amount_of_teams % 2 == 0 ? $even = 'even' : $even = 'odd';
-		// $even == 'even' ? $even_verified = 'yes' : $even_verified = 'nah';
-		// $return['even_no_teams'] = $even;
-		
 		$buys = amountOfBuys($amount_of_teams);
-		$return['amount_of_teams'] = $amount_of_teams;
-		$return['amount_of_teams_first_round'] = $amount_of_teams -  $buys;
+		$amount_of_teams_first_round = $amount_of_teams -  $buys;
+
+		$return['amount_of_teams_for_ideal'] = $amount_of_teams + $buys;
+		$return['amount_of_teams_entered'] = $amount_of_teams;
+		$return['amount_of_teams_first_round'] = $amount_of_teams_first_round;
 		$return['amount_of_teams_first_round_buys'] = $buys;
-		
-		// $return['amount_of_second_round_games'] = $amount_of_teams / 4;
+		$return['amount_of_teams_second_round'] = ($amount_of_teams_first_round / 2 ) + $buys;
 		// $return['amounts_for_ideal_bracket'] = array(128,64,32,16,8,4);
-		
 		return $return;
 	}
 
@@ -67,7 +78,7 @@ mypr(whatWeNeedToKnow($a));
 	
 	function amountOfBuys($amount_of_teams){
 		#amount of teams that get first round buy..
-		$first_round_buys = array( 5 => 3, 6 => 2, 7 => 1, 9 => 7, 10 => 6, 11 => 5, 12 => 4, 13 => 3, 14 => 2, 15 => 1, 17 => 15, 18 => 14, 19 => 13, 20 => 12, 21 => 11, 22 => 10, 23 => 9, 24 => 8, 25 => 7, 26 => 6, 27 => 5, 28 => 4, 29 => 3, 30 => 2, 31 => 1);
+		$first_round_buys = array( 5 => 3, 6 => 2, 7 => 1, 9 => 7, 10 => 6, 11 => 5, 12 => 4, 13 => 3, 14 => 2, 15 => 1, 17 => 15, 18 => 14, 19 => 13, 20 => 12, 21 => 11, 22 => 10, 23 => 9, 24 => 8, 25 => 7, 26 => 6, 27 => 5, 28 => 4, 29 => 3, 30 => 2, 31 => 1, 33 => 31, 34 => 30, 35 => 29, 36 => 28, 37 => 27, 38 => 26, 39 => 25, 40 => 24, 41 => 23, 42 => 22, 43 => 21, 44 => 20, 45 => 19, 46 => 18, 47 => 17, 48 => 16, 49 => 15, 50 => 14, 51 => 13, 52 => 12, 53 => 11, 54 => 10, 55 => 9, 56 => 8, 57 => 7, 58 => 6, 59 => 5, 60 => 4, 61 => 3, 62 => 2, 63 => 1 );
 
 		if (array_key_exists($amount_of_teams, $first_round_buys)) {
 			return $first_round_buys["$amount_of_teams"];
