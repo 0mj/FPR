1bbbd12c35d7074c458c7a1f9f4abf3d265dd4ed
Jamison <mattjamison@bravowell.com>
2019-01-03 12:58:29 -0500

    site-footer class

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<footer class="footer">
+<footer class="site-footer">
     <div class="container">
         <br>
         <span class="text-muted">{{ date('d M Y H:i',strtotime('now'))}} UTC <a href="mailto:MattJamisonXYZ@gmail.com" id="footerEmail">@Matt</a></span>
