f38919932228639a14cc128a2b7a250ac2500253
JAMISON <thirdshiftvideo@gmail.com>
2019-02-07 11:46:52 -0500

    Removed dead end routes.

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,12 @@
 	| routes are loaded by the RouteServiceProvider within a group which
 	| contains the "web" middleware group. Now create something great!
 	|
-
-	 
-
-	 Route::get('/', function () {
-	     return '<h1>' . strtotime('now') . '</h1>';
-	 });
-
 */
 
 
 Route::get('/', function () {
     return view('index');
 })->name('index.index');
-
-
-
-# PONGERS..
-// Route::get('pongers/getAllPongers', 'PongerController@getAllPongers');
-// Route::view('pongers/getAllPongers', 'PongerController@getAllPongers');
-// Route::view('pongers/getAllPongers', 'pongers.getAllPongers');
-
-
-
 
 # https://laravel.com/docs/5.5/controllers#restful-partial-resource-routes
 # register many resource controllers at once by passing an array to the resources method:
@@ -41,45 +24,6 @@ Route::resources([
 	'brackets'=> 'BracketController',
 	
 ]);
-Route::get('pong', function () {
-	    return view('pongers.test');
-	});
-
-// Route::view('badgeviewer/', 'badgeviewer');
-Route::get('badgeviewer', function () {
-	    return view('badgeviewer.index');
-	});
-
-Route::get('pongbracket', function () {
-	     return view('pongbracket.bracket');
-	 });
-
-# LEAGUES..
-// Route::get('leagues', 'LeagueController@index');
-// Route::resource('leagues', 'LeagueController');
-
-# BRACKET..
-Route::get('bracket', 'BracketController@index');
-Route::get('about', 'BracketController@about');
-
-
-
-
-# PONGERS ALL TEST..
-	// Route::get('foo', function(){	return 'hello world'; 	});
-	// Route::get('/', 'PongerController@getAllPongers');
-	// Route::get('/getAllpongers', 'PongerController@getAllPongers');
-	
-	# THIS WORKS.
-	// Route::get('/', function () {
-	//     return view('pongers.getAllpongers');
-	// });
-
-
-
-
-
-
 
 
 Auth::routes();
