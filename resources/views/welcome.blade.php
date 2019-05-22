2004b11aad726a2502bcabffb5eff382c3fdc077
Matt Jamison <thirdshiftvideo@gmail.com>
2019-05-22 14:11:26 -0400

    Added nav and layout sections to /react-quick-sand

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,27 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>sdlc</title>
+
+
+@section('title', 'Qmatt')
+
+@section('main')
+    
+        
+        
+        
+
+        
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
          <link href="/css/app.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
+    
+    
     
         <div id="example"></div>
         
-        <script src="/js/app.js"></script>    
-    </body>
-</html>
+        <script src="/js/app.js"></script>
+        
+@endsection
